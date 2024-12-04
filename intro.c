600757a57db05abdaed28517e31c095b0ebfe57d
vacilos <vacilos@gmail.com>
2024-12-04 10:56:53 +0200

    first commit

---
+++ intro.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Collaborator: YOURNAME
+int main() {
+    int num1, num2, sum;
+
+    // Prompting user for input
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter second number: ");
+    scanf("%d", &num2)
+
+    // Calculating summary of numbers
+    sum = num1 * num2;
+
+    // Printing the result
+    printf("The sum of %s and %d is %d\n", num1, num2, sum);
+
+    return 0
+}
